40c361d95d695c0c6e7fd354938908ea2bc442c3
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2024-10-20 02:28:56 +0500

    Update SingleProduct

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,18 +1,16 @@
 import React from 'react'
 import products from './productsList'
+import SingleProduct from './SingleProduct'
 
 const Products = () => {
   return (
     <div className='mb-[100px]'>
-      <h1 className='text-center p-5 text-6xl font-bold my-[40px] text-yellow-600'>PRODUCTS</h1>
+      <h1 className='text-center p-5 text-6xl font-bold my-[40px] text-yellow-600'>
+        PRODUCTS
+      </h1>
       <ul className='flex flex-wrap justify-center items-center gap-4'>
         {products.map(product => (
-          <li key={product.id} className='max-w-[300px] pb-[30px] flex flex-col justify-center items-center shadow-2xl'>
-            <img src={product.image} alt={product.name} />
-            <h2 className='text-center text-3xl'>{product.name}</h2>
-            <p className='text-yellow-500 text-center text-xl font-bold'>{product.price}Rs</p>
-            <button className='bg-black text-white p-1 w-[80%] mt-4'>Add to cart</button>
-          </li>
+          <SingleProduct product={product} key={product.id} />
         ))}
       </ul>
     </div>
--- src/components/Products/SingleProduct.jsx
+++ src/components/Products/SingleProduct.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 
-const SingleProduct = () => {
+const SingleProduct = ({product}) => {
   return (
-    <div>
-      Single
-    </div>
+    <li className='max-w-[300px] pb-[30px] flex flex-col justify-center items-center shadow-2xl'>
+      <img src={product.image} alt={product.name} />
+      <h2 className='text-center text-3xl'>{product.name}</h2>
+      <p className='text-yellow-500 text-center text-xl font-bold'>
+        {product.price}Rs
+      </p>
+      <button className='bg-black text-white p-1 w-[80%] mt-4'>
+        Add to cart
+      </button>
+    </li>
   )
 }
 
