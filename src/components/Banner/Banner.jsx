968780349cc7eac1991d62c1375a78cbc96b1cba
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2024-10-19 23:16:55 +0500

    Add Banner

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import banner from '../../assets/banner.webp'
+
+const Banner = () => {
+  return (
+    <>
+      <img src={banner} alt="home page banner" className='w-full' /> 
+    </>
+  )
+}
+
+export default Banner
