36cccb3f7a6a4340387e591c2d8a403bf545d740
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2024-10-19 22:58:08 +0500

    Add Initial Commit with Header

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import logo from '../../assets/logo.png'
+import { FaShoppingCart } from 'react-icons/fa'
+import navlinks from './navlinks'
+
+const Navbar = () => {
+  return (
+    <nav className='flex justify-around items-center py-3 bg-black'>
+      <img src={logo} alt='adorn aura logo' className='w-[100px]' />
+      <ul className='flex gap-3'>
+        {
+          navlinks.map((link, index) => (
+            <li key={index}>
+              <a href={link.href} className='text-white text-xl font-bold'>
+                {link.label}
+              </a>
+            </li>
+          ))
+        }
+      </ul>
+      <a href='/cart' className='text-white text-3xl'>
+        <FaShoppingCart />
+      </a>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ src/components/Navbar/navlinks.js
@@ -0,0 +1,6 @@
+export default [
+  { href: '/', label: 'Home' },
+  { href: '/about', label: 'About' },
+  { href: '/products', label: 'Products' },
+  { href: '/contact', label: 'Contact' }
+]
