59097146b2a07c60d6e61a305b8cb577100f1892
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2024-11-20 06:41:10 +0500

    Remove more code

--- src/components/Products/SingleProduct.jsx
+++ src/components/Products/SingleProduct.jsx
@@ -1,39 +1,20 @@
-import React, { useEffect, useRef } from 'react'
-import { getLocalCart } from '../../utils/cartLocalStorage'
+import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-const SingleProduct = ({ product }) => {
-  const countRender = useRef(0)
-
-  const handleRemItems = () => {
-    const localCartProducts = getLocalCart() || []
-    const qty = localCartProducts.filter(p => p.id == product.documentId)
-    setProductRem(qty[0]?.quantity || 0)
-  }
-
-  useEffect(() => {
-    if (countRender.current === 0) {
-      handleRemItems()
-      countRender.current += 1
-    }
-  }, [])
-  return (
-    <NavLink
-      to={`/product/${product.documentId}`}
-      className='max-w-[300px] pb-[30px] flex flex-col justify-center items-center shadow-2xl'
-    >
-      <img src={product.imgUrl} alt={product.title} className='mb-[10px]' />
-      <h2 className='text-center text-3xl'>{product.title}</h2>
-      <div className='flex items-end gap-3'>
-        {product.oldPrice > 0 && (
-          <span className='text-red-600 line-through'>
-            {product.oldPrice}Rs
-          </span>
-        )}
-        <p className='text-yellow-500 text-xl font-bold'>{product.price}Rs</p>
-      </div>
-    </NavLink>
-  )
-}
+const SingleProduct = ({ product }) => (
+  <NavLink
+    to={`/product/${product.documentId}`}
+    className='max-w-[300px] pb-[30px] flex flex-col justify-center items-center shadow-2xl'
+  >
+    <img src={product.imgUrl} alt={product.title} className='mb-[10px]' />
+    <h2 className='text-center text-3xl'>{product.title}</h2>
+    <div className='flex items-end gap-3'>
+      {product.oldPrice > 0 && (
+        <span className='text-red-600 line-through'>{product.oldPrice}Rs</span>
+      )}
+      <p className='text-yellow-500 text-xl font-bold'>{product.price}Rs</p>
+    </div>
+  </NavLink>
+)
 
 export default SingleProduct
