8f05dcc70168a648c823d53cec2502ec9380bbcf
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2024-11-28 05:00:39 +0500

    Worked on Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,6 +5,7 @@ import { NavLink } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import SideCart from './SideCart'
 import { toggleMiniCart } from '../../store/cart/cart'
+// import Example from './NavTest'
 
 const Navbar = () => {
   const cart = useSelector(store => store.cart.cart)
@@ -14,27 +15,27 @@ const Navbar = () => {
 
   return (
     <>
-      <nav className='flex justify-between items-center px-7 py-4 bg-black fixed top-0 left-0 right-0 z-10'>
+      <nav className='flex justify-between items-center px-7 h-[97px] bg-black fixed top-0 left-0 right-0 z-10'>
         <NavLink to={'/'} className='mr-1 w-[100px]'>
           <img src={logo} alt='adorn aura logo' className='w-[100px]' />
         </NavLink>
         <div className='flex items-center gap-5'>
-          {/* <form className='flex items-center gap-3'>
+          <form className='flex items-center gap-3'>
             <input
               type='text'
               placeholder='Search...'
-              className='indent-2 p-1 rounded outline-none'
+              className='indent-2 p-1 rounded-[40px] outline-none'
             />
             <button type='submit'>
               <FaSearch className='text-white' />
             </button>
-          </form> */}
+          </form>
           <button
             onClick={toggleSidebar}
-            className='text-white text-[40px] relative'
+            className='text-white text-[30px] relative'
           >
             {cart.length > 0 && (
-              <span className='bg-red-500 text-white text-[11px] font-bold rounded-[50%] py-1 px-2 absolute top-[-6px] left-[29px]'>
+              <span className='bg-red-500 text-white text-[8px] font-bold rounded-[50%] py-1 px-2 absolute top-[-6px] left-[25px]'>
                 {cart.length}
               </span>
             )}
